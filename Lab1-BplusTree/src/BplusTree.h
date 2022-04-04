36e457bab6766241c3dd01044315963a86dc7a46
Jose Huby <jose.huby@utec.edu.pe>
2022-04-03 21:41:58 -0500

    Delete: avance

--- Lab1-BplusTree/src/BplusTree.h
+++ Lab1-BplusTree/src/BplusTree.h
@@ -17,7 +17,9 @@
 #define PARENT(i) mem[i].parent
 #define IS_LEAF(i) (mem[i].child[0] == null)
 #define IS_NOT_LEAF(i) (mem[i].child[0] != null)
+#define NEXT(i) mem[i].child[degree]
 #define NEW_NODE mem.size(); mem.emplace_back()
+#define CAN_GIVE(i) (SIZE(i) > min)
 
 class BplusTree{
     using Key = uint32_t;
@@ -26,9 +28,10 @@ class BplusTree{
     using Size = uint32_t;
 
     static constexpr Index null = 0; //La posición 0 en el vector es dummy para permitir null = 0
-    static constexpr Size degree = 21; //Muchisimo mas rapido con grado 6
+    static constexpr Size degree = 21; //Más rapido con grado 6
     static constexpr Size capacity = degree-1;
     static constexpr Size mid = degree/2;
+    static constexpr Size min = (degree/2) + (degree%2) - 1;
     
     struct Node{
         Size size;
@@ -86,8 +89,11 @@ class BplusTree{
         Index other_half = NEW_NODE;
         SIZE(other_half) = degree - mid;
         memcpy(&(KEY(other_half,0)), &(KEY(current,mid)), sizeof(Key)*(degree - mid));
+        NEXT(other_half) = NEXT(current);
+        NEXT(current) = other_half;
                     //Se hace split y el centro se queda en el nodo hoja y tambien sube
-        
+                    //Se actualiza la double linked list
+
         do{
             if(current == root){
                 root = NEW_NODE;
@@ -124,12 +130,8 @@ class BplusTree{
         }while(true);
     }
 
-
-    void borrar(Key const& key){
-
-    }
-    std::vector<Key> bfs(){
-        std::vector<Key> result;
+    std::vector<int> bfs(){
+        std::vector<int> result;
         if(root == null) return result;
         std::queue<Index> q;
         q.push(root);
@@ -146,7 +148,199 @@ class BplusTree{
         }
         return result;
     }
+
     int nodes(){return mem.size();}
+
+
+    void borrar(Key const& key){
+        if(root == null) return; //Si no hay root, termina
+
+        int i;
+        int level = 0; //Head del stack
+        int child[10]; //Stack de childs
+        Index parent[10]; //Stack de parents
+        Index current = root;
+        Index internal = null;
+        while(IS_NOT_LEAF(current)){
+            for(i = 0; i< SIZE(current); ++i){
+                if(key < KEY(current, i)) break;
+                else if(key == KEY(current, i)) internal = current;
+            }
+            child[level] = i;
+            parent[level++] = current;
+            current = CHILD(current, i);
+        } //Encontrar nodo hoja y el camino de parents
+
+        
+        int ileft = i - 1;
+        int iright = i + 1;
+
+
+        for(i = 0; i< SIZE(current); ++i){
+            if(key == KEY(current, i)) break;
+        } //Encontrar key en nodo hoja
+        
+
+        if(i == SIZE(current)) return; //Si no hay key, termina
+
+        SIZE(current) -= 1;
+        for(; i< SIZE(current); ++i){
+           KEY(current, i) = KEY(current, i+1);
+        } //Eliminar el key en nodo hoja
+        
+        
+
+        if(current == root){
+            if(SIZE(root) == 0) root = null; //Si el root está vacio, libera memoria
+            return;
+        } //Si solo esta el root, termina
+
+
+        if(internal == null && SIZE(current) >= min) return; // Si no hay copia interna y cumple el tamaño, termina 
+
+
+        Index sibling;
+        if(ileft >= 0){
+            sibling = CHILD(parent[level], ileft);
+            if(CAN_GIVE(sibling)){
+                for(int j = SIZE(current); j > 0; --j){
+                    KEY(current, j) = KEY(current, j - 1);
+                }
+                KEY(current, 0) = KEY(sibling, SIZE(sibling));
+                SIZE(current) += 1;
+                SIZE(sibling) -= 1;
+                KEY(parent[level], ileft) = KEY(current, 0);
+                return;
+            }
+        }
+        if(iright <= SIZE(parent[level])){
+            sibling = CHILD(parent[level], iright);
+            if(CAN_GIVE(sibling)){
+                KEY(current, SIZE(current)) = KEY(sibling, 0);
+                SIZE(current) += 1;
+                SIZE(sibling) -= 1;
+                for(int j = 0; j < SIZE(sibling); --j){
+                    KEY(sibling, j) = KEY(sibling, j + 1);
+                }
+                KEY(parent[level], iright - 1) = KEY(sibling, 0);
+                return;
+            }
+        } //Si algun hermano puede prestar, presta, arregla al padre y termina
+
+        Index deleted;
+        if(ileft >= 0){
+            sibling = CHILD(parent[level], ileft);
+            for(int j = 0; j < SIZE(current); --j){
+                KEY(sibling, SIZE(sibling) + j) = KEY(current, j);
+            }
+            SIZE(sibling) += SIZE(current);
+            NEXT(sibling) = NEXT(current);
+            deleted = current;
+            i = ileft;
+        }
+        else if(iright <= SIZE(parent[level])){
+            sibling = CHILD(parent[level], ileft);
+            for(int j = 0; j < SIZE(sibling); --j){
+                KEY(current, SIZE(current) + j) = KEY(sibling, j);
+            }
+            SIZE(current) += SIZE(sibling);
+            NEXT(current) = NEXT(sibling);
+            deleted = sibling;
+            i = iright - 1;
+
+        } //Si existe algun hermano y ninguno puede prestar, se juntan
+
+        do{
+            current = parent[level];
+
+            if(current == root && SIZE(current) == 1){
+                root = (deleted == CHILD(current, 0)) ? CHILD(current, 1) : CHILD(current, 0);
+                return;
+            } // Si es root y tiene size 1, se cambia de root el hijo no eliminado
+            
+            for(int j = i; j< SIZE(current); ++j){
+                KEY(current, j) = KEY(current, j+1);
+            } // Se elimina la llave
+            
+            int remove_child = 0;
+            for(; remove_child <= SIZE(current); ++remove_child){
+                if(remove_child == deleted) break;
+            }
+            for(; remove_child < SIZE(current); ++remove_child){
+                CHILD(current, remove_child) =  CHILD(current, remove_child + 1);
+            }
+            CHILD(current, SIZE(current)) = null;
+            SIZE(current) -= 1; //Se remueve el hijo eliminado
+
+            if(current == root) return; //Si es root, termina
+
+            if(SIZE(current) >= min) return; //Si no hace underflow, termina
+
+            ileft = child[level] - 1;
+            iright = child[level] + 1;
+            level -= 1;
+            if(ileft >= 0){
+                sibling = CHILD(parent[level], ileft);
+                if(CAN_GIVE(sibling)){
+                    for(int j = SIZE(current); j > 0; --j){
+                        KEY(current, j) = KEY(current, j - 1);
+                    }
+                    KEY(current, 0) = KEY(parent[level], ileft);
+                    KEY(parent[level], ileft) = KEY(sibling, SIZE(sibling) - 1);
+                    for(int j = SIZE(current) + 1; j > 0; --j){
+                        CHILD(current, j) = CHILD(current, j - 1);
+                    }
+                    
+                    SIZE(current) += 1;
+                    SIZE(sibling) -= 1;
+                    return;
+                }
+            }
+            if(iright <= SIZE(parent[level])){
+                sibling = CHILD(parent[level], iright);
+                if(CAN_GIVE(sibling)){
+                    KEY(current, SIZE(current)) = KEY(sibling, 0);
+                    SIZE(current) += 1;
+                    SIZE(sibling) -= 1;
+                    for(int j = 0; j < SIZE(sibling); --j){
+                        KEY(sibling, j) = KEY(sibling, j + 1);
+                    }
+                    KEY(parent[level], iright - 1) = KEY(sibling, 0);
+                    return;
+                }
+            } //Si algun hermano puede prestar, presta, arregla al padre y termina
+
+            if(ileft >= 0){
+                sibling = CHILD(parent[level], ileft);
+                for(int j = 0; j < SIZE(current); --j){
+                    KEY(sibling, SIZE(sibling) + j) = KEY(current, j);
+                }
+                SIZE(sibling) += SIZE(current);
+                NEXT(sibling) = NEXT(current);
+                deleted = current;
+                i = ileft;
+            }
+            else if(iright <= SIZE(parent[level])){
+                sibling = CHILD(parent[level], ileft);
+                for(int j = 0; j < SIZE(sibling); --j){
+                    KEY(current, SIZE(current) + j) = KEY(sibling, j);
+                }
+                SIZE(current) += SIZE(sibling);
+                NEXT(current) = NEXT(sibling);
+                deleted = sibling;
+                i = iright - 1;
+
+            } //Si existe algun hermano y ninguno puede prestar, se juntan
+
+        }while(true);
+        
+
+
+
+
+        
+
+    }
 };
 
 #endif
